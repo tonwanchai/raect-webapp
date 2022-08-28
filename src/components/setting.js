0d5585e7e9f4dbc07407f722003f2d86ad722985
tonwanchai <grand38326@gmail.com>
2022-08-28 21:04:44 +0700

    Add responsive to Setting page

--- src/components/setting.js
+++ src/components/setting.js
@@ -6,6 +6,7 @@ import {
   MenuItem,
   FormControl,
   Select,
+  Grid,
 } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
@@ -15,9 +16,18 @@ import AddBoxIcon from "@mui/icons-material/AddBox";
 import AddDialog from "./addDialog";
 import EditDialog from "./editDialog";
 import { createFruit, getFruits } from "../functions";
+
 import DeleteIcon from "@mui/icons-material/Delete";
-import { deleteFruit, getFruitQueue, createFruitQueue, createQueue, deleteQueueByID, deleteAllQueue, updateQueueFruit } from "../api";
-import { useHistory } from 'react-router-dom'
+import {
+  deleteFruit,
+  getFruitQueue,
+  createFruitQueue,
+  createQueue,
+  deleteQueueByID,
+  deleteAllQueue,
+  updateQueueFruit,
+} from "../api";
+import { useHistory } from "react-router-dom";
 
 const Item = styled(Button)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -38,7 +48,7 @@ export default function Setting(props) {
   const [addSelectBox, setAddSelectBox] = useState([]);
   const [loading, setLoading] = useState(false);
   const [queue, setQueue] = useState([]);
-  const history = useHistory()
+  const history = useHistory();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -83,47 +93,44 @@ export default function Setting(props) {
 
   const handleClickAddQueue = () => {
     setQueue([...queue, { fruitID: " " }]);
-
   };
 
   const handleClickDeleteQueue = (index) => {
-    console.log(queue)
-    console.log(index)
-    let arrQueue = queue
-    arrQueue.splice(index,1)
-    console.log(arrQueue)
-    setQueue([...arrQueue])
-  }
-
-  const handleChangeSelectBox = (e,index) => {
-    let arrQueue = queue
-    arrQueue[index].fruitID = e.target.value
-    console.log(arrQueue[index], index)
-    setQueue([...arrQueue])
-
-  }
-
-  const handleClickConfirm = async() => {
-    console.log("create queue", queue)
-    const result = createQueue(queue)
-    window.location.reload();
-  }
-
-  const handleClickDeleteWithIndex = async(id) => {
-    const result = deleteQueueByID(id)
-    window.location.reload();
-  }
-
-  const handleClickDeleteAll = () => {
-    const result = deleteAllQueue()
-    window.location.reload();
-  }
-
-  const handleButtonBack = pageURL => {
-    history.push(pageURL);
-    window.location.reload()
+    console.log(queue);
+    console.log(index);
+    let arrQueue = queue;
+    arrQueue.splice(index, 1);
+    console.log(arrQueue);
+    setQueue([...arrQueue]);
   };
 
+  const handleChangeSelectBox = (e, index) => {
+    let arrQueue = queue;
+    arrQueue[index].fruitID = e.target.value;
+    console.log(arrQueue[index], index);
+    setQueue([...arrQueue]);
+  };
+
+  const handleClickConfirm = async () => {
+    console.log("create queue", queue);
+    const result = createQueue(queue);
+    window.location.reload();
+  };
+
+  const handleClickDeleteWithIndex = async (id) => {
+    const result = deleteQueueByID(id);
+    window.location.reload();
+  };
+
+  const handleClickDeleteAll = () => {
+    const result = deleteAllQueue();
+    window.location.reload();
+  };
+
+  const handleButtonBack = (pageURL) => {
+    history.push(pageURL);
+    window.location.reload();
+  };
 
   if (!loading) return <>loading</>;
   return (
@@ -131,9 +138,159 @@ export default function Setting(props) {
       {/* <Box>
         <Button size="large" onClick={() =>handleButtonBack('/')}>{"<==="}</Button>
       </Box> */}
-      <br/>
-      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Box sx={{ border: "1px line grey", backgroundColor:'#d9d9d9', width: "45%", height: 824 }}>
+      <br />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={{ xs: 1, md: 3 }}
+          columns={12}
+          
+        >
+          <Grid item xs={12} md={6} key={1}>
+            <Box
+              sx={{
+                border: "1px line grey",
+                backgroundColor: "#d9d9d9",
+                width: "80%",
+                height: 824,
+                overflow: "auto",
+              }}
+            >
+              <br />
+              <Stack spacing={2} sx={{ width: "80%", margin: "auto" }}>
+                {fruits.map((fruit) => (
+                  <Box fullWidth key={fruit._id} sx={{ display: "flex" }}>
+                    <Box sx={{ width: "90%" }}>
+                      <Item
+                        key={fruit._id}
+                        onClick={() => handleClickOpenEditItemButton(fruit)}
+                      >
+                        {fruit.name}
+                      </Item>
+                    </Box>
+                    <Box sx={{ width: "10%" }}>
+                      <IconButton
+                        color="error"
+                        disableFocusRipple
+                        disableTouchRipple
+                        aria-label="add"
+                        style={{ fontSize: 30, backgroundColor: "transparent" }}
+                        onClick={() => handleClickDeleteFruit(fruit._id)}
+                      >
+                        <DeleteIcon fontSize="inherit" />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                ))}
+                <IconButton
+                  disableFocusRipple
+                  disableTouchRipple
+                  aria-label="add"
+                  color="primary"
+                  style={{ fontSize: 30, backgroundColor: "transparent" }}
+                  onClick={handleClickOpenAddItemButton}
+                >
+                  <AddBoxIcon fontSize="inherit" />
+                </IconButton>
+              </Stack>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6} key={2}>
+            <Box
+              sx={{
+                backgroundColor: "#d9d9d9",
+                width: "80%",
+                height: 824,
+                overflow: "auto",
+                marginLeft: "auto"
+              }}
+            >
+              <br />
+
+              <Stack spacing={2} sx={{ width: "80%", margin: "auto" }}>
+                {queue.map(
+                  (data, index) =>
+                    data.fruitID !== "undefined" && (
+                      <Box fullWidth sx={{ display: "flex" }}>
+                        <Box sx={{ width: "90%" }}>
+                          <FormControl fullWidth>
+                            <InputLabel
+                              sx={{
+                                marginTop: "5px",
+                                backgroundColor: "transparent",
+                              }}
+                              id="demo-simple-select-label"
+                            >
+                              {index + 1}
+                            </InputLabel>
+                            <Select
+                              labelId="fruit-queue"
+                              id={data._id}
+                              value={data.fruitID}
+                              sx={{
+                                marginTop: "5px",
+                                backgroundColor: "#FFFFFF",
+                              }}
+                              size="small"
+                              label="Fruit name"
+                              onChange={(e) => handleChangeSelectBox(e, index)}
+                            >
+                              {fruits.map((fruit) => (
+                                <MenuItem value={fruit._id}>
+                                  {fruit.name}
+                                </MenuItem>
+                              ))}
+                            </Select>
+                          </FormControl>
+                        </Box>
+                        <Box sx={{ width: "10%" }}>
+                          <IconButton
+                            color="error"
+                            disableFocusRipple
+                            disableTouchRipple
+                            aria-label="add"
+                            style={{
+                              fontSize: 30,
+                              backgroundColor: "transparent",
+                            }}
+                            // onClick={() => handleClickDeleteQueue(index)}
+                            onClick={() => handleClickDeleteWithIndex(data._id)}
+                          >
+                            <DeleteIcon fontSize="inherit" />
+                          </IconButton>
+                        </Box>
+                      </Box>
+                    )
+
+                    // <></>
+                )}
+                <IconButton
+                  disableFocusRipple
+                  disableTouchRipple
+                  aria-label="add"
+                  color="primary"
+                  onClick={handleClickAddQueue}
+                  style={{ fontSize: 30, backgroundColor: "transparent" }}
+                >
+                  <AddBoxIcon fontSize="inherit" />
+                </IconButton>
+              </Stack>
+              <Box sx={{ width: "100%", display: "flex" }}>
+                <Box sx={{ margin: "auto", marginTop: "5px" }} fullWidth>
+                  <Button variant="contained" onClick={handleClickConfirm}>
+                    Confirm
+                  </Button>{" "}
+                  <Button variant="contained" onClick={handleClickDeleteAll}>
+                    Delete All
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+      {/* <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ border: "1px line grey", backgroundColor:'#d9d9d9', width: "45%", height: 824, overflow:'auto' }}>
           <br />
           <Stack spacing={2} sx={{ width: "80%", margin: "auto" }}>
             {fruits.map((fruit) => (
@@ -173,7 +330,7 @@ export default function Setting(props) {
             </IconButton>
           </Stack>
         </Box>
-        <Box sx={{ backgroundColor:'#d9d9d9', width: "45%", height: 824 }}>
+        <Box sx={{ backgroundColor:'#d9d9d9', width: "45%", height: 824, overflow:'auto'  }}>
           <br />
 
           <Stack spacing={2} sx={{ width: "80%", margin: "auto" }}>
@@ -236,7 +393,7 @@ export default function Setting(props) {
             </Box>
           </Box>
         </Box>
-      </Box>
+      </Box> */}
 
       {openAddItem && (
         <AddDialog open={openAddItem} onClose={handleCloseAddItemButton} />
