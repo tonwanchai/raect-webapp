e20fb37174dc3bcfce350cca698a706e28b0b687
tonwanchai <grand38326@gmail.com>
2022-08-31 05:55:30 +0700

    Change Header layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,53 @@
 import * as React from "react";
-import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import HelpIcon from "@mui/icons-material/Help";
-import IconButton from "@mui/material/IconButton";
-import Link from "@mui/material/Link";
-import MenuIcon from "@mui/icons-material/Menu";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import Box from "@mui/material/Box";
-
-import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
-import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
 import { withRouter } from "react-router-dom";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 const lightColor = "rgba(255, 255, 255, 0.7)";
 
 function Header(props) {
   const { history } = props;
+  const checkResponsive = useMediaQuery("(min-width:900px)");
+  const pages = ["Products", "Pricing", "Blog"];
+  const settings = ["Profile", "Account", "Dashboard", "Logout"];
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleButtonClick = pageURL => {
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const handleButtonClick = (pageURL) => {
+    setAnchorElNav(null);
     history.push(pageURL);
-    window.location.reload()
+    window.location.reload();
   };
 
   const handleSignOut = () => () => {
-    localStorage.removeItem("access_token")
-    window.location.reload()
-  }
+    localStorage.removeItem("access_token");
+    window.location.reload();
+  };
   // const handleButtonBack = pageURL => {
   //   history.push(pageURL);
   //   window.location.reload()
@@ -40,33 +57,150 @@ function Header(props) {
     <React.Fragment>
       <AppBar
         color="primary"
+        sx={{ height: "70px", bgcolor: "#75582B" }}
         position="static"
-        sx={{ height: "70px" }}
         elevation={0}
       >
-        <Toolbar>
-          <Grid container spacing={1} alignItems="center" />
-          <Tooltip disableHoverListener title={"tab"}>
-            <Box sx={{display:'flex', width:'50%', flexGrow:1}}>
-              <Button
-                variant="contained"
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {checkResponsive && (
+              <img
+                src="./images/Icon.png"
+                alt="icon"
+                width={56}
+                height={56}
+                style={{
+                  display: { xs: "none", md: "flex" },
+                  mr: 1,
+                  marginTop: "-10px",
+                }}
+              />
+            )}
+            {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "kanit",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+                marginLeft: "10px",
+              }}
+            >
+              FRUIT
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
                 size="large"
-                sx={{ margin: "10px", bgcolor: "#c1c1c1" }}
-                onClick={() => handleButtonClick(window.location.pathname == '/' ? "/setting":"/")}
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-                {window.location.pathname == '/' ? "Setting":"Home"}
-              </Button>
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                <MenuItem key={"setting"} onClick={handleCloseNavMenu}>
+                  <Typography
+                    textAlign="center"
+                    fontSize={20}
+                    onClick={() =>
+                      handleButtonClick(
+                        window.location.pathname == "/" ? "/setting" : "/"
+                      )
+                    }
+                  >
+                    {window.location.pathname == "/" ? "Setting" : "Home"}
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+            {!checkResponsive && (
+              <img
+                src="./images/Icon.png"
+                alt="icon"
+                width={56}
+                height={56}
+                style={{
+                  display: { xs: "flex", md: "none" },
+                  mr: 1,
+                  marginTop: "4px",
+                }}
+              />
+            )}
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+                marginLeft: "10px",
+              }}
+            >
+              FRUIT
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               <Button
-               variant="contained"
-               size="large"
-               sx={{ margin: "10px", bgcolor: "#c1c1c1" }}
-               onClick={handleSignOut()}
+                key={11}
+                onClick={() =>
+                  handleButtonClick(
+                    window.location.pathname == "/" ? "/setting" : "/"
+                  )
+                }
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                Sign out
+                <Typography variant="a" fontSize={20}>
+                  {window.location.pathname == "/" ? "Setting" : "Home"}
+                </Typography>
               </Button>
             </Box>
-          </Tooltip>
-        </Toolbar>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip disableHoverListener title="Open settings">
+                <Typography
+                  fontSize={20}
+                  sx={{ cursor: "pointer", marginBottom: '3px'}}
+                  onClick={handleSignOut()}
+                >
+                  Sign out
+                </Typography>
+              </Tooltip>
+            </Box>
+          </Toolbar>
+        </Container>
       </AppBar>
     </React.Fragment>
   );
